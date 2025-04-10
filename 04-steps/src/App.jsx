1b5557d84e3259543a3c58b8b72966f6850a0db2
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-04-10 22:02:48 +0000

    Create a step component

--- 04-steps/src/App.jsx
+++ 04-steps/src/App.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./index.css";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
+  const [step, setStep] = useState(1)
+  const [reset, setReset] = useState(false)
+
+  function previousStep(){
+    setStep(step => step - 1)
+  }
+  function nextStep(){
+    setStep(step => step + 1)
+  }
+
+  function resetState(){
+    setReset(!reset);
+    setStep(1)
+  }
+
   return (
-    <div>App</div>
-  )
+    <div className="steps">
+      <>
+        {!reset ? (
+          <>
+            <div className="numbers">
+              <div className={step == 1 ? "active" : ""}>1</div>
+              <div className={step == 2 ? "active" : ""}>2</div>
+              <div className={step == 3 ? "active" : ""}>3</div>
+            </div>
+
+            <div className="message">
+              <span>Step {step}: </span> {messages[step - 1]}
+            </div>
+
+            <div className="buttons">
+              <button className="btn" onClick={previousStep}>
+                Previous
+              </button>
+              <button className="btn" onClick={nextStep}>
+                Next
+              </button>
+            </div>
+          </>
+        ) : (
+          <Reset />
+        )}
+        <div className="buttons">
+          <button
+            className="btn"
+            style={{
+              backgroundColor: !reset ? "red" : "blueviolet",
+              marginTop: "3rem",
+            }}
+            onClick={resetState}
+          >
+            {!reset ? "Reset" : "Home"}
+          </button>
+        </div>
+      </>
+    </div>
+  );
 }
 
 export default App
+
+
+function Reset() {
+  return (
+    <div className="reset">
+      <p>I am a reset component 😂, I destroy states.</p>
+    </div>
+  )
+}
