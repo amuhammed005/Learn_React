47a495e7effeab6a491395a80c0fb7bfc35708af
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-04-10 23:59:55 +0000

    little change

--- 04-steps/src/App.jsx
+++ 04-steps/src/App.jsx
@@ -8,84 +8,81 @@ const messages = [
 ];
 
 function App() {
-  const [step, setStep] = useState(1)
-  const [isOpen, setIsOpen] = useState(true)
-  const [reset, setReset] = useState(false)
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  const [reset, setReset] = useState(false);
 
-  function previousStep(){
-    if (step < 3) setStep(step - 1);
+  function previousStep() {
+    if (step > 1) setStep(step - 1);
   }
-  function nextStep(){
-    if (step > 1) setStep(step + 1)
+  function nextStep() {
+    if (step < 3) setStep(step + 1);
   }
 
-  function resetState(){
+  function resetState() {
     setReset(!reset);
-    setStep(1)
+    setStep(1);
   }
 
-  function handleIsOpen(){
-    setIsOpen(!isOpen)
+  function handleIsOpen() {
+    setIsOpen(!isOpen);
   }
 
   return (
-      <>
-          <div className="is-open">
-            <button onClick={handleIsOpen}>{isOpen ? "❌" : "✖"}</button>
+    <>
+      <div className="is-open">
+        <button onClick={handleIsOpen}>{isOpen ? "❌" : "✖"}</button>
+      </div>
+      {isOpen && (
+        <div className="steps">
+          {!reset ? (
+            <>
+              <div className="numbers">
+                <div className={step == 1 ? "active" : ""}>1</div>
+                <div className={step == 2 ? "active" : ""}>2</div>
+                <div className={step == 3 ? "active" : ""}>3</div>
+              </div>
+
+              <div className="message">
+                <span>Step {step}: </span> {messages[step - 1]}
+              </div>
+
+              <div className="buttons">
+                <button className="btn" onClick={previousStep}>
+                  Previous
+                </button>
+                <button className="btn" onClick={nextStep}>
+                  Next
+                </button>
+              </div>
+            </>
+          ) : (
+            <Reset />
+          )}
+          <div className="buttons">
+            <button
+              className="btn"
+              style={{
+                backgroundColor: !reset ? "red" : "blueviolet",
+                marginTop: "3rem",
+              }}
+              onClick={resetState}
+            >
+              {!reset ? "Reset" : "Home"}
+            </button>
           </div>
-      { 
-      isOpen &&     
-      <div className="steps">
-        {!reset ? (
-          <>
-            <div className="numbers">
-              <div className={step == 1 ? "active" : ""}>1</div>
-              <div className={step == 2 ? "active" : ""}>2</div>
-              <div className={step == 3 ? "active" : ""}>3</div>
-            </div>
-
-            <div className="message">
-              <span>Step {step}: </span> {messages[step - 1]}
-            </div>
-
-            <div className="buttons">
-              <button className="btn" onClick={previousStep}>
-                Previous
-              </button>
-              <button className="btn" onClick={nextStep}>
-                Next
-              </button>
-            </div>
-          </>
-        ) : (
-          <Reset />
-        )}
-        <div className="buttons">
-          <button
-            className="btn"
-            style={{
-              backgroundColor: !reset ? "red" : "blueviolet",
-              marginTop: "3rem",
-            }}
-            onClick={resetState}
-          >
-            {!reset ? "Reset" : "Home"}
-          </button>
         </div>
-       
-    </div>
-      }
-    </> 
+      )}
+    </>
   );
 }
 
-export default App
-
+export default App;
 
 function Reset() {
   return (
     <div className="reset">
       <p>I am a reset component 😂, I destroy states.</p>
     </div>
-  )
+  );
 }
