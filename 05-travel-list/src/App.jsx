b39b102fe54c3669cb3fa894cf614262df5d477e
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-04-13 23:56:59 +0000

    small touches on footer section and css

--- 05-travel-list/src/App.jsx
+++ 05-travel-list/src/App.jsx
@@ -189,22 +189,25 @@ function Item({ item, onTogglePacked, onDelete }) {
   );
 }
 function Stats({items}){
+  if(!items.length) return <p className='stats'>
+    <em>Start adding some items to your packing list 🚀</em>
+  </p>
+
   const totalItems = items.length
   const packedItems = items.filter(item=>item.packed).length
-  const percentage = totalItems === 0 ? 0 : Math.round((packedItems / totalItems) * 100)
+  const percentage = Math.round((packedItems / totalItems) * 100)
   return (
     <footer className="stats">
       <em>
-        {totalItems > 0 ? (
-          <span>
-            You have {totalItems} item{totalItems > 1 ? "s" : ""} on your list,
-            {packedItems > 0
-              ? `and you already parked ${packedItems}(${percentage}%)`
-              : " you have not packed any yet!"}
-          </span>
-        ) : (
-          <span> You have no items in your packing list </span>
-        )}
+        {percentage === 100
+          ? "You got everything packed. Ready to go ✈"
+          : `You have ${totalItems} item${
+              totalItems > 1 ? "s" : ""
+            } on your list, ${
+              packedItems > 0
+                ? `and you already parked ${packedItems}(${percentage}%)`
+                : " you have not packed any yet!"
+            }`}
       </em>
     </footer>
   );
