89d49d8854e6cdd33b8cfa2d111fcaced5cc012d
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-04-14 22:15:59 +0000

    Add functionality and enhance ui

--- challenge-4-accordian-component/src/App.jsx
+++ challenge-4-accordian-component/src/App.jsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react'
 
 const faqs = [
   {
+    id: 1,
     title: "Where are these chairs assembled?",
     text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
   },
   {
+    id: 2,
     title: "How long do I have to return my chair?",
     text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
   },
   {
+    id: 3,
     title: "Do you ship to countries outside the EU?",
     text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
   },
@@ -24,20 +27,29 @@ export default function App() {
 }
 
 function Accordian(){
+  const [selectItem, setSelectedItem] = useState(null)
+
+  function handleClickItem(id){
+    setSelectedItem(currentId=>currentId !== id ? id : null)
+  }
+
   return (
     <ul>
-      {faqs.map(faq=><Faq key={faq.title} faq={faq} />)}
+      {faqs.map((faq) => (
+        <li
+          className={`faq ${selectItem === faq.id ? "active" : ""}`}
+          onClick={() => handleClickItem(faq.id)}
+        >
+          <div className="faq-header">
+            <span className={selectItem === faq.id ? "active-id" : ""}>
+              {faq.id <= 9 ? "0" + faq.id : faq.id}
+            </span>
+            <h4 className="title">{faq.title}</h4>
+            <button>{selectItem === faq.id ? "-" : "+"}</button>
+          </div>
+          {selectItem === faq.id && <p className="faq-text">{faq.text}</p>}
+        </li>
+      ))}
     </ul>
-  )
-}
-
-function Faq({faq}){
-  
-  return (
-    <div className="faq">
-      <span>01</span>
-      <p>{faq.title}</p>
-      <button>+</button>
-    </div>
   );
 }
