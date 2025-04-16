85b11a4a2af4faeca6f00bfd4497bb4151b41f36
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-04-16 02:04:19 +0000

    controlled elements and full functionality implemented

--- challenge-5-tip-calculator/src/App.jsx
+++ challenge-5-tip-calculator/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function App() {
   return (
@@ -9,30 +10,87 @@ export default function App() {
 }
 
 function Calculator(){
-  return (
-    <div className="app">
-      <div>
-        <label htmlFor="">How much was the bill? </label>
-        <input type="text" placeholder="Ghc" value={"user"} />
+  const [bill, setBill] = useState(0)
+  const [userService, setUserService] = useState("Disatisfied");
+  const [friendService, setFriendService] = useState("Satisfied");
+
+  let userTip = 0;
+  let friendTip = 0;
+  if (userService === "disatisfied") {
+     userTip += 0;
+  } else if (userService === "satisfied") {
+    userTip += 5
+  } else if (userService === "very-satisfied") {
+    userTip += 10;
+  }
+
+  if (friendService === "disatisfied") {
+    friendTip += 0;
+  } else if (friendService === "satisfied") {
+    friendTip += 5;
+  } else if (friendService === "very-satisfied") {
+    friendTip += 10;
+  }
+
+  const totalTip = (userTip + friendTip) / 2
+  const totalBill = bill + totalTip
+
+  console.log("bill: " + typeof(bill))
+  console.log( "user tip: " + typeof(userTip))
+  console.log("friend tip: " + typeof(friendTip))
+  console.log( "total tip: " + typeof(totalTip))
+  console.log( "total bill: " + typeof(totalBill))
+
+  function handleReset(){
+    setBill(0)
+    setUserService("Disatisfied")
+    setFriendService("Disatisfied")
+  }
+
+    return (
+      <div className="app">
+        <div>
+          <label htmlFor="">How much was the bill? </label>
+          <input
+            type="number"
+            placeholder="0.00"
+            value={bill}
+            onChange={(e) => setBill(Number(e.target.value))}
+          />
+        </div>
+        <div>
+          <label htmlFor="">How did you like the service? </label>
+          <select
+            name="user"
+            value={userService}
+            onChange={(e) => setUserService(e.target.value)}
+          >
+            <option value="disatisfied">Disatisfied 0%</option>
+            <option value="satisfied">Satisfied 5%</option>
+            <option value="very-satisfied">Very satisfied 10%</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="">How did your friend like the service? </label>
+          <select
+            name="friend"
+            value={friendService}
+            onChange={(e) => setFriendService(e.target.value)}
+          >
+            <option value="disatisfied">Disatisfied 0%</option>
+            <option value="satisfied">Satisfied 5%</option>
+            <option value="very-satisfied">Very satisfied 10%</option>
+          </select>
+        </div>
+
+        {totalBill > 0 && (
+          <div>
+            <p>
+              Your total bill is Gh₵{totalBill} (Gh₵{bill} + Gh₵{totalTip}){" "}
+            </p>
+            <button className='btn' onClick={handleReset}>Reset</button>
+          </div>
+        )}
       </div>
-      <div>
-        <label htmlFor="">How did you like the service? </label>
-        <select name="user" value="user">
-          <option value="disatisfied">Disatisfied 0%</option>
-          <option value="satisfied">Satisfied 5%</option>
-          <option value="very">Very satisfied 10%</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="">How did your friend like the service? </label>
-        <select name="friend" value="friend">
-          <option value="disatisfied">Disatisfied 0%</option>
-          <option value="satisfied">Satisfied 5%</option>
-          <option value="very">Very satisfied 10%</option>
-        </select>
-      </div>
-      <p>Your total bill is Ghc50(Ghc bill + Ghc tip) </p>
-      <button>Reset</button>
-    </div>
-  );
+    );
 }
