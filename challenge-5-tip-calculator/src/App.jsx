2200b7f1771df2ac7af7a1dcdefc866ea20c3e0f
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-04-16 01:12:26 +0000

    Little style to enhance UI

--- challenge-5-tip-calculator/src/App.jsx
+++ challenge-5-tip-calculator/src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
 
 function Calculator(){
   return (
-    <div>
+    <div className="app">
       <div>
         <label htmlFor="">How much was the bill? </label>
         <input type="text" placeholder="Ghc" value={"user"} />
@@ -18,17 +18,17 @@ function Calculator(){
       <div>
         <label htmlFor="">How did you like the service? </label>
         <select name="user" value="user">
-          <option value="">Disatisfied 0%</option>
-          <option value="">Satisfied 5%</option>
-          <option value="">Very satisfied 10%</option>
+          <option value="disatisfied">Disatisfied 0%</option>
+          <option value="satisfied">Satisfied 5%</option>
+          <option value="very">Very satisfied 10%</option>
         </select>
       </div>
       <div>
         <label htmlFor="">How did your friend like the service? </label>
         <select name="friend" value="friend">
-          <option value="">Disatisfied 0%</option>
-          <option value="">Satisfied 5%</option>
-          <option value="">Very satisfied 10%</option>
+          <option value="disatisfied">Disatisfied 0%</option>
+          <option value="satisfied">Satisfied 5%</option>
+          <option value="very">Very satisfied 10%</option>
         </select>
       </div>
       <p>Your total bill is Ghc50(Ghc bill + Ghc tip) </p>
