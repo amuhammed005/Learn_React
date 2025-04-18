7552a961cc1b4e7d11fdbbe1b3690d5ea542305c
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-04-18 23:20:06 +0000

    Update form Functionality for seamless user experience

--- task-1-shopping-list/src/App.jsx
+++ task-1-shopping-list/src/App.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
-
 
 const initialItems = [
   { id: 1, name: "Milk", quantity: 2, purchased: false },
@@ -9,19 +8,21 @@ const initialItems = [
   { id: 4, name: "Tomatoes", quantity: 6, purchased: false },
 ];
 
-
 export default function App() {
-  const [toggleAddForm, setToggleAddForm] = useState(false)
+  const [toggleAddForm, setToggleAddForm] = useState(false);
   const [selectedOption, setSelectedOption] = useState("all");
-  const [items, setItems] = useState(initialItems)
+  const [items, setItems] = useState(() => {
+    const storedItems = localStorage.getItem("items");
+    return storedItems ? JSON.parse(storedItems) : initialItems;
+  });
 
   function handleAddItem(item) {
     setItems((items) => [...items, item]);
-    setToggleAddForm(false)
+    setToggleAddForm(false);
   }
 
-  function handleOpenForm(){
-    setToggleAddForm(open=>!open)
+  function handleOpenForm() {
+    setToggleAddForm((open) => !open);
   }
 
   function handleTogglePurchased(id) {
@@ -30,16 +31,15 @@ export default function App() {
         item.id === id ? { ...item, purchased: !item.purchased } : item
       )
     );
-    setItems((items) =>
-      items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
-    );
   }
 
-  function handleDeleteItem(id){
-    setItems(items.filter(item=>item.id !== id))
+  function handleDeleteItem(id) {
+    setItems(items.filter((item) => item.id !== id));
   }
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
 
   return (
     <>
@@ -67,10 +67,10 @@ export default function App() {
   );
 }
 
-function Navbar(){
+function Navbar() {
   return (
-    <nav className='nav'>
-      <h2 className='logo'>GroceryStore</h2>
+    <nav className="nav">
+      <h2 className="logo">GroceryStore</h2>
 
       <div>
         <a href="">Home</a>
@@ -78,33 +78,34 @@ function Navbar(){
         <a href="">Cart</a>
       </div>
     </nav>
-  )
+  );
 }
 
-function Button({children, onClick}){
+function Button({ children, onClick }) {
   return (
-    <button className='btn' onClick={onClick}>{children}</button>
-  )
+    <button className="btn" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
-function FilterItems({selectedOption, setSelectedOption}){
-    return (
-      <div className="filter">
-        <label>Filter by choice</label>
-        <select
-          value={selectedOption}
-          onChange={(e) => setSelectedOption(e.target.value)}
-        >
-          <option value="all">All</option>
-          <option value="purchased">Purchased</option>
-          <option value="not-purchased">Not purchased</option>
-        </select>
-      </div>
-    );
+function FilterItems({ selectedOption, setSelectedOption }) {
+  return (
+    <div className="filter">
+      <label>Filter by choice</label>
+      <select
+        value={selectedOption}
+        onChange={(e) => setSelectedOption(e.target.value)}
+      >
+        <option value="all">All</option>
+        <option value="purchased">Purchased</option>
+        <option value="not-purchased">Not purchased</option>
+      </select>
+    </div>
+  );
 }
 
 function ItemsList({ items, selectedOption, onTogglePurchased, onDeleteItem }) {
-
   let filteredItems;
   if (selectedOption === "all") filteredItems = items;
   if (selectedOption === "purchased") {
@@ -117,21 +118,25 @@ function ItemsList({ items, selectedOption, onTogglePurchased, onDeleteItem }) {
   return (
     <div className="items">
       <h2>List of Items</h2>
-      <ul>
-        {filteredItems.map((item) => (
-          <Item
-            key={item.id}
-            item={item}
-            onTogglePurchased={onTogglePurchased}
-            onDeleteItem={onDeleteItem}
-          />
-        ))}
-      </ul>
+      {filteredItems.length === 0 ? (
+        <p>No items found</p>
+      ) : (
+        <ul>
+          {filteredItems.map((item) => (
+            <Item
+              key={item.id}
+              item={item}
+              onTogglePurchased={onTogglePurchased}
+              onDeleteItem={onDeleteItem}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
 
-function Item({item, onTogglePurchased, onDeleteItem}){
+function Item({ item, onTogglePurchased, onDeleteItem }) {
   return (
     <li>
       <h4>{item.name}</h4>
@@ -142,7 +147,7 @@ function Item({item, onTogglePurchased, onDeleteItem}){
           value={item.purchased}
           onChange={() => onTogglePurchased(item.id)}
         />
-        <button className="button" onClick={()=>onDeleteItem(item.id)}>
+        <button className="button" onClick={() => onDeleteItem(item.id)}>
           <MdDelete />
         </button>
       </div>
@@ -150,27 +155,27 @@ function Item({item, onTogglePurchased, onDeleteItem}){
   );
 }
 
-function FormAddItem({items, onAddItem}){
-  const [name, setName] = useState("")
-  const [quantity, setQuantity] = useState("")
+function FormAddItem({ items, onAddItem }) {
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("");
 
-  function handleFormSubmit(e){
-    e.preventDefault()
+  function handleFormSubmit(e) {
+    e.preventDefault();
 
     if (!name || !quantity) return;
 
-    const newId = items.length + 1
+    const newId = items.length + 1;
     const newItem = {
       id: newId,
       name,
       quantity,
       purchased: false,
-    }
-    console.log(newItem)
-    onAddItem(newItem)
+    };
+    console.log(newItem);
+    onAddItem(newItem);
 
-    setName("")
-    setQuantity("")
+    setName("");
+    setQuantity("");
   }
 
   return (
@@ -179,6 +184,7 @@ function FormAddItem({items, onAddItem}){
         <label>Name</label>
         <input
           type="text"
+          required
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
@@ -187,6 +193,8 @@ function FormAddItem({items, onAddItem}){
         <label>Quantity</label>
         <input
           type="number"
+          min="1"
+          required
           value={quantity}
           onChange={(e) => setQuantity(e.target.value)}
         />
