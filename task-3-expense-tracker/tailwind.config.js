284104d583a78a128b5a01f6c56d490866293a6c
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-04-21 21:30:50 +0000

    configured tailwind css

---
+++ task-3-expense-tracker/tailwind.config.js
@@ -0,0 +1,12 @@
+// /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // important for React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
