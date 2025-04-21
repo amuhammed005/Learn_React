45bdfa1d423d2224a8543682907e9974ebe486f5
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-04-21 23:32:47 +0000

    Added form submit functionality

--- task-3-expense-tracker/src/App.jsx
+++ task-3-expense-tracker/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const data = [
+const dummyData = [
   {
     id: 1,
     transaction: "Cash",
@@ -19,12 +19,20 @@ const data = [
 ];
 
 export default function App() {
-  return <div className='w-3/4 md:w-2/6 flex flex-col mx-auto my-10'>
-    <Logo />
-    <Balance />
-    <History data={data} />
-    <Transaction />
-  </div>;
+  const [data, setData] = useState([])
+
+  function handleAddTransaction(newItem){
+    setData([...data, newItem])
+  }
+
+  return (
+    <div className="w-3/4 md:w-2/6 flex flex-col mx-auto my-10">
+      <Logo />
+      <Balance />
+      <History data={data} />
+      <Transaction onAddTransaction={handleAddTransaction} />
+    </div>
+  );
 }
 
 
@@ -57,46 +65,84 @@ function Balance(){
 
 function History({data}){
   return (
-    <div className='mb-8'>
-      <h3 className="font-medium uppercase pb-2 border-b-2 border-b-gray-400">History</h3>
-      <div className='mt-6 flex flex-col gap-3'>
-        {
-          data.map(item=><Item item={item} key={item.id} />)
-        }
-      </div>
+    <div className="mb-8">
+      <h3 className="font-medium uppercase pb-2 border-b-2 border-b-gray-400">
+        History
+      </h3>
+      {data.length === 0 ? (
+        <p className="mt-2">
+          No transaction history! <em>Start a new transaction.</em>
+        </p>
+      ) : (
+        <div className="mt-6 flex flex-col gap-3">
+          {data.map((item) => (
+            <Item item={item} key={item.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
 function Item({item}){
   return (
-    <div className='flex items-center justify-between border-r-4 border-green-600 p-3 shadow-md rounded-sm'>
-      <p>{item.transaction}</p>
+    <div className="flex items-center justify-between border-r-4 border-green-600 p-3 shadow-md rounded-sm">
+      <p>
+        {item.transaction &&
+          item.transaction.charAt(0).toUpperCase() +
+            item.transaction.slice(1).toLowerCase()}
+      </p>
       <span>{item.amount}</span>
     </div>
-  )
+  );
 }
 
-function Transaction(){
+function Transaction({onAddTransaction}){
+  const [transaction, setTransaction] = useState("")
+  const [amount, setAmount ] = useState("")
+
+  function handleSumbit(e){
+    e.preventDefault()
+    if(!transaction || !amount ) return;
+
+    const newTransaction = {
+      id: crypto.randomUUID(),
+      transaction,
+      amount: Number(amount),
+    }
+    onAddTransaction(newTransaction)
+
+    console.log(typeof(amount))
+
+    console.log(newTransaction)
+
+    setTransaction("")
+    setAmount("")
+  }
+
   return (
     <div>
       <h3 className="mb-3 font-medium uppercase pb-2 border-b-2 border-b-gray-400">
         Add new transaction
       </h3>
-      <form className="">
+      <form className="" onSubmit={handleSumbit}>
         <label htmlFor="transaction">Transaction</label>
         <input
           type="text"
+          value={transaction}
+          onChange={e=>setTransaction(e.target.value)}
           className="w-full mb-3 p-2 focus:outline-none border"
           placeholder="Enter transaction name..."
         />
 
         <label htmlFor="amount" className="flex flex-col">
-          Amount{" "}
+          Amount
           <span className="text-sm">(negative-expense, positive-income)</span>
         </label>
         <input
           type="number"
+          value={amount}
+          onChange={e=>setAmount(Number(e.target.value))}
           className="w-full mb-3 p-2 focus:outline-none border"
           placeholder="Enter amount..."
         />
