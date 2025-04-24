165fae5cab105f645a7469c1ea0b20eeec69bd67
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-04-24 16:43:26 +0000

    complete functionality of text-expander resuable component

--- challenge-7-text-expander/src/App.jsx
+++ challenge-7-text-expander/src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <TextExpander
+        text="Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled it to
+              make a type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially
+              unchanged. It was popularised in the 1960s with the release of Letraset
+              sheets containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of Lorem
+              Ipsum."
+        minLength={80}
+        collapsedByDefault={true}
+      />
+      <TextExpander
+        text="Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled it to
+              make a type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially
+              unchanged. It was popularised in the 1960s with the release of Letraset
+              sheets containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of Lorem
+              Ipsum."
+        minLength={180}
+        collapsedByDefault={false}
+        expandLabel='Less'
+        collapseLabel='More'
+      />
+      <TextExpander
+        text="Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled it to
+              make a type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially
+              unchanged. It was popularised in the 1960s with the release of Letraset
+              sheets containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of Lorem
+              Ipsum."
+        minLength={300}
+        collapsedByDefault={true}
+      />
+    </div>
+  );
 }
 
-export default App
+function TextExpander({text=`It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.`, minLength=40, collapsedByDefault=true, expandLabel="Show less", collapseLabel="Show more" }){
+  const [isCollapsed, setIsCollapsed] = useState(collapsedByDefault)
+  const isLong = text.length > minLength
+  const displayText = !isLong ? text : isCollapsed ? text.slice(0, minLength).trim() + "..." : text;
+  
+  const toggle = () => setIsCollapsed(prev=>!prev)
+
+  const showToggle = isLong
+
+  return (
+    <p>
+      {displayText}
+
+      <button
+        className="toggle-button"
+        style={{ color: isCollapsed ? "blue" : "red" }}
+        onClick={toggle}
+      >
+        {showToggle && isCollapsed ? collapseLabel : expandLabel}
+      </button>
+    </p>
+  );
+}
